2510f08f18ea7d74c212225e1e3f81424d823aba
Rezaul Hasan <Rezaul Hasan>
2016-08-19 10:06:19 +0600

    testing

---
+++ app-api/src/main/java/app/roconmachine/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package app.roconmachine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by asus on 8/18/2016.
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
