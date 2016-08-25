ff9644adc3c4d5867a7079283c196bd48849c251
Rezaul Hasan <Rezaul Hasan>
2016-08-25 18:30:21 +0600

    trying to run a rest controller

--- app-api/src/main/java/app/roconmachine/api/endpoint/GreetingController.java
+++ app-api/src/main/java/app/roconmachine/api/endpoint/GreetingController.java
@@ -11,12 +11,13 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/ping")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
